c899d681355ad30b6ea4113ae32cb1eea8b5fcd6
rabia <m1ny00n7177@gmail.com>
2024-07-16 12:18:04 +0300

    sign-in

--- src/main/java/com/peaksoft/gadgetarium/service/UserService.java
+++ src/main/java/com/peaksoft/gadgetarium/service/UserService.java
@@ -1,11 +1,12 @@
 package com.peaksoft.gadgetarium.service;
 
 import com.peaksoft.gadgetarium.mapper.AuthMapper;
-import com.peaksoft.gadgetarium.model.dto.response.UserRequest;
-import com.peaksoft.gadgetarium.model.dto.response.UserResponse;
 import com.peaksoft.gadgetarium.model.dto.request.UserSignInRequest;
 import com.peaksoft.gadgetarium.model.dto.response.UserLoginResponse;
+import com.peaksoft.gadgetarium.model.dto.response.UserRequest;
+import com.peaksoft.gadgetarium.model.dto.response.UserResponse;
 import com.peaksoft.gadgetarium.model.entities.User;
+import com.peaksoft.gadgetarium.model.enums.Role;
 import com.peaksoft.gadgetarium.repository.UserRepository;
 import com.peaksoft.gadgetarium.security.jwt.JwtUtil;
 import lombok.AccessLevel;
@@ -16,6 +17,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,6 +29,7 @@ public class UserService {
     UserRepository userRepository;
     JwtUtil jwtUtil;
     AuthenticationManager authenticationManager;
+    BCryptPasswordEncoder passwordEncoder;
     AuthMapper authMapper;
 
     public UserLoginResponse login(UserSignInRequest request) {
@@ -50,6 +53,9 @@ public class UserService {
 
     public UserResponse createUser(UserRequest request) {
         User user = authMapper.mapToUser(request);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setConfirm_the_password(passwordEncoder.encode(request.getConfirmThePassword()));
+        user.setRole(Role.USER);
         userRepository.save(user);
         log.info("Successfully created User " + user.getId());
         return authMapper.mapToResponse(user);
