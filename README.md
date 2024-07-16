b64f08bee02a56a5920b39635e30d49f5dbd40ad
Aida <baatyrbekovaaida.dev@gmail.com>
2024-07-16 17:42:37 +0300

    migration

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ![Gadjetarium.diagramm-Page-1.drawio.png](Gadjetarium.diagramm-Page-1.drawio.png)
+admin=$2a$12$21dcsWqrwZuVaBst7YU/9uLY8IhkCVuRyB68JG/QOM5I45YaAG.ry
+user1=$2a$12$t9LPvZjsr04gTWJSB886dOS5bzpI5uW3Hm/nRnQltQ5awCqKuY3fK
+user2=$2a$12$.0uBxKL//rIxaSOxxLWozeoD9WZj1574XZ8lfu35pJIFTJhg12eAC
+
+
+
---
+++ src/main/resources/db/db.sql
@@ -0,0 +1,64 @@
+insert into users(create_date, confirm_the_password, email, gender, last_name, local, name, password, phone_number,
+                  role)
+values ('05-12-2023', '$2a$12$21dcsWqrwZuVaBst7YU/9uLY8IhkCVuRyB68JG/QOM5I45YaAG.ry', 'admin@gmail.com', 'male',
+        'Adminov', 'ky_KG', 'Admin',
+        '$2a$12$21dcsWqrwZuVaBst7YU/9uLY8IhkCVuRyB68JG/QOM5I45YaAG.ry',
+        '+796657646',
+        'ADMIN'),
+       ('05-03-2024', '$2a$12$t9LPvZjsr04gTWJSB886dOS5bzpI5uW3Hm/nRnQltQ5awCqKuY3fK', 'user@gmail.com', 'female',
+        'User1', 'ky_KG', 'User1',
+        '$2a$12$t9LPvZjsr04gTWJSB886dOS5bzpI5uW3Hm/nRnQltQ5awCqKuY3fK', '+799987689',
+        'USER'),
+       ('2024-07-10', '$2a$12$.0uBxKL//rIxaSOxxLWozeoD9WZj1574XZ8lfu35pJIFTJhg12eAC', 'user1@gmail.com', 'male',
+        'User2', 'ky_KG', 'User2',
+        '$2a$12$.0uBxKL//rIxaSOxxLWozeoD9WZj1574XZ8lfu35pJIFTJhg12eAC', '+709786756',
+        'USER');
+
+insert into payments(payment_systems, card_number, cvc, month_date, year_date, user_name)
+
+values ('VISA', '1234567812345678', '123', 7, 2024, 'Admin'),
+       ('MASTERCARD', '8765432187654321', '456', 12, 2025, 'User1'),
+       ('МИР', '123412341234123', '789', 6, 2023, 'User2');
+
+insert into brands(brand_name)
+values ('Apple'),
+       ('Samsung');
+
+insert into categories (electronic_type)
+values ('Mobile Phones'),
+       ('Laptops'),
+       ('Tablets');
+
+insert into sub_categories(category_id, name_of_sub_category)
+values (1, 'Smartphones'),
+       (1, 'Feature Phones'),
+       (1, 'Gaming Laptops'),
+       (1, 'Ultrabooks'),
+       (1, 'Android Tablets'),
+       (1, 'iOS Tablets');
+
+insert into products (id, product_name, product_status, category_id, memory, color, operation_memory, screen,
+    operation_system, operation_system_num, date_of_release, sim_card, processor, weight,
+    guarantee, rating, discount, price, create_date, brand_id)
+
+           values ( 1, 'iPhone 14', 'AVAILABLE', 1, '64GB', 'BLACK', '4GB', '6.1 дюйма',
+             'iOS', '16.0', '2023-09-01', 'Dual SIM', 'A15 Bionic', 174,
+             '1 год', '4.5', 10, 99900, '2024-07-15', 1),
+
+            ( 2, 'Galaxy S23', 'AVAILABLE', 1, '128GB', 'WHITE', '8GB', '6.5 дюйма',
+             'Android', '13', '2023-02-11', 'Single SIM', 'Snapdragon 8 Gen 2', 196,
+             '2 года', '4.7', 15, 89900, '2024-07-15', 2 );
+
+-- insert into basket_products(basket_id,product_id)
+-- values (),
+--
+--
+-- insert into baskets()
+-- values ();
+--
+-- insert into deliveries (delivery_status, order_time, user_id)
+-- VALUES ('PICKUP_FROM_STORE', '2024-07-10', 1),
+--        ('DELIVERY_BY_COURIER', '2024-08-01', 2),
+--        ('PICKUP_FROM_STORE', '2024-09-15', 3);
+--
+--
