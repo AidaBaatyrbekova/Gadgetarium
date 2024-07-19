293b5a2aafb246d7242dab6fea68b4268f67a349
Asus Zenbook <anapiiaevsandary@gmail.com>
2024-07-19 22:45:15 +0300

    sign-up exceptions

---
+++ src/main/java/com/peaksoft/gadgetarium/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.peaksoft.gadgetarium.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+    @ControllerAdvice
+    public class GlobalExceptionHandler {
+
+        @ExceptionHandler(UserAlreadyExistsException.class)
+        public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException exception){
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        @ExceptionHandler(UsernameNotFoundException.class)
+        public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        }
+
+        @ExceptionHandler(Exception.class)
+        public ResponseEntity<String> handleGlobalException(Exception ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
