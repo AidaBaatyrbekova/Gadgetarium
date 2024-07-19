e983f45aabc9457fe2df597559c90c4ecaed81b2
aidaiysmaiylova <aidaiysmaiylova31@gmail.com>
2024-07-19 22:47:51 +0600

    sign-up-with-google

--- src/main/java/com/peaksoft/gadgetarium/controller/AuthController.java
+++ src/main/java/com/peaksoft/gadgetarium/controller/AuthController.java
@@ -1,9 +1,9 @@
 package com.peaksoft.gadgetarium.controller;
 
-import com.peaksoft.gadgetarium.model.dto.response.UserRequest;
+import com.peaksoft.gadgetarium.model.dto.request.LoginRequest;
+import com.peaksoft.gadgetarium.model.dto.request.UserRequest;
+import com.peaksoft.gadgetarium.model.dto.response.LoginResponse;
 import com.peaksoft.gadgetarium.model.dto.response.UserResponse;
-import com.peaksoft.gadgetarium.model.dto.request.UserSignInRequest;
-import com.peaksoft.gadgetarium.model.dto.response.UserLoginResponse;
 import com.peaksoft.gadgetarium.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
@@ -33,12 +33,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public UserLoginResponse login(@RequestBody UserSignInRequest request) {
+    public LoginResponse login(@RequestBody LoginRequest request) {
         return userService.login(request);
-    }
-
-    @PutMapping("/updatePassword")
-    public void updatePassword(@RequestBody UserUpdatePasswordRequest request) {
-        userService.updatePassword(request);
     }
 }
--- src/main/java/com/peaksoft/gadgetarium/exception/NotFoundException.java
+++ src/main/java/com/peaksoft/gadgetarium/exception/NotFoundException.java
@@ -1,23 +1,8 @@
 package com.peaksoft.gadgetarium.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@RestControllerAdvice
-public class NotFoundException {
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException e) {
-        Map<String, String> errors = new HashMap<>();
-        e.getBindingResult().getFieldErrors().forEach((error) -> {
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
-        return errors;
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
     }
 }
