e914f1ff239e79cde19c9fd2aa80d739449a8961
Aida <baatyrbekovaaida.dev@gmail.com>
2024-07-23 01:42:58 +0300

    migration

--- src/main/resources/db/db.sql
+++ src/main/resources/db/db.sql
@@ -20,44 +20,56 @@ values ('VISA', '1234567812345678', '123', 7, 2024, 'Admin'),
        ('MASTERCARD', '8765432187654321', '456', 12, 2025, 'User1'),
        ('МИР', '123412341234123', '789', 6, 2023, 'User2');
 
-insert into brands(brand_name)
-values ('Apple'),
-       ('Samsung');
+insert into brands (brand_name)
+values ('Samsung'),
+       ('Apple'),
+       ('OnePlus'),
+       ('Huawei');
 
 insert into categories (electronic_type)
-values ('Mobile Phones'),
+values ('Smartphones'),
        ('Laptops'),
-       ('Tablets');
+       ('Tablets'),
+       ('Accessories');
 
-insert into sub_categories(category_id, name_of_sub_category)
+
+insert into sub_categories (category_id, name_of_sub_category)
 values (1, 'Smartphones'),
        (1, 'Feature Phones'),
-       (1, 'Gaming Laptops'),
        (1, 'Ultrabooks'),
+       (1, 'Gaming Laptops'),
        (1, 'Android Tablets'),
-       (1, 'iOS Tablets');
+       (1, 'iOS Tablets'),
+       (1, 'Smartwatches'),
+       (1, 'Fitness Bands');
 
+insert into products (create_date, discount, price, weight, brand_id, category_id, color,
+                      date_of_release, guarantee, memory,
+                      operation_memory, operation_system, operation_system_num,
+                      processor, product_name, product_status,
+                      rating, screen, sim_card)
+values ('2023-07-01', 1000, 69999, 150, 1, 1, 'BLACK', '2023-06-15', '24 months', 'GB_128', 'GB_8', 'ANDROID', '11.0',
+        'Snapdragon 888', 'Galaxy S21', 'NEW_DEVICES', '4.5', '6.2 inches', 'Dual'),
+       ('2023-06-15', 1500, 99999, 160, 1, 1, 'SILVER', '2023-05-20', '24 months', 'GB_256', 'GB_12', 'IOS', '14.5',
+        'A14 Bionic', 'iPhone 12 Pro', 'SALES', '4.7', '6.1 inches', 'Single'),
+       ('2023-05-30', 5, 79999, 155, 1, 1, 'WHITE', '2023-04-10', '12 months', 'GB_128', 'GB_8', 'ANDROID', '11.0',
+        'Exynos 2100', 'Galaxy S21+', 'BY_REDUCING_THE_PRICE', '4.6', '6.7 inches', 'Dual'),
+       ('2023-07-10', 20, 119999, 180, 1, 1, 'BLUE', '2023-07-01', '24 months', 'GB_512', 'GB_16', 'ANDROID', '12.0',
+        'Snapdragon 895', 'OnePlus 9 Pro', 'BY_INCREASING_THE_PRICE', '4.8', '6.7 inches', 'Dual'),
+       ('2023-06-25', 8, 89999, 165, 1, 1, 'GREEN', '2023-06-01', '24 months', 'GB_256', 'GB_12', 'ANDROID', '11.0',
+        'Kirin 9000', 'Huawei P40 Pro', 'SALES', '4.6', '6.6 inches', 'Dual');
 
-insert into products(create_date, discount, price, weight, brand_id, category_id, color, date_of_release, guarantee,
-                     memory, operation_memory,
-                     operation_system, operation_system_num, processor, product_name, product_status, rating, screen,
-                     sim_card)
-values ('17-07-2024', 10, 10000, 200, 1, 1, 'BLACK', '01-01-2022', '1 Year', 'GB_128', 'GB_16', 'IOS', '10',
-        'A15 Bionic', 'iPhone 13', 'NEW_DEVICES', '4', '6.1', 'Nano-SIM'),
-       ('17-07-2024', 15, 90000, 300, 1, 2, 'WHITE', '01-01-2023', '2 Year', 'GB_512', 'GB_16', 'IOS', '10',
-        'A10 Bionic', 'iPAD PRO','SALES', '5', '6.5', 'Nano-SIM');
+insert into baskets (id)
+values (1),
+       (2),
+       (3),
+       (4);
 
-
--- insert into basket_products(basket_id,product_id)
--- values (),
---
---
--- insert into baskets()
--- values ();
---
--- insert into deliveries (delivery_status, order_time, user_id)
--- VALUES ('PICKUP_FROM_STORE', '2024-07-10', 1),
---        ('DELIVERY_BY_COURIER', '2024-08-01', 2),
---        ('PICKUP_FROM_STORE', '2024-09-15', 3);
---
---
+insert into basket_products (basket_id, product_id)
+values (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (1, 6),
+       (1, 7);
