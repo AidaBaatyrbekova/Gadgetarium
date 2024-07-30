75f1958a260e83c4e495fd1154a45829f6b4eb9d
Aida <baatyrbekovaaida.dev@gmail.com>
2024-07-31 00:14:13 +0300

    Main-page

--- src/main/java/com/peaksoft/gadgetarium/controller/MainPageController.java
+++ src/main/java/com/peaksoft/gadgetarium/controller/MainPageController.java
@@ -1,6 +1,9 @@
 package com.peaksoft.gadgetarium.controller;
+
 import com.peaksoft.gadgetarium.service.ProductService;
 import com.peaksoft.gadgetarium.model.dto.response.ProductResponse;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,30 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-    @RestController
-    @RequestMapping("/api/main")
-    public class MainPageController {
+@RestController
+@RequestMapping("/api/main")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class MainPageController {
+    ProductService productService;
 
-        private final ProductService productService;
-
-        @Autowired
-        public MainPageController(ProductService productService) {
-            this.productService = productService;
-        }
-
-        @GetMapping("/discounted")
-        public List<ProductResponse> getDiscountedProducts() {
-            return productService.findDiscountedProducts();
-        }
-
-        @GetMapping("/new-arrivals")
-        public List<ProductResponse> getNewArrivals() {
-            return productService.findNewArrivals();
-        }
-
-        @GetMapping("/recommended")
-        public List<ProductResponse> getRecommendedProducts() {
-            return productService.findRecommendedProducts();
-        }
+    @Autowired
+    public MainPageController(ProductService productService) {
+        this.productService = productService;
     }
+
+    @GetMapping("/discounted")
+    public List<ProductResponse> getDiscountedProducts() {
+        return productService.findDiscountedProducts();
+    }
+
+    @GetMapping("/new-arrivals")
+    public List<ProductResponse> getNewArrivals() {
+        return productService.findNewArrivals();
+    }
+
+    @GetMapping("/recommended")
+    public List<ProductResponse> getRecommendedProducts() {
+        return productService.findRecommendedProducts();
+    }
+}
 
