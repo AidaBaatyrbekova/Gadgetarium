b17951b08f538f748eb304d24d2b08fe6b116769
Asus Zenbook <anapiiaevsandary@gmail.com>
2024-08-01 21:04:23 +0300

    swagger documentation

--- src/main/java/com/peaksoft/gadgetarium/controller/AuthController.java
+++ src/main/java/com/peaksoft/gadgetarium/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.peaksoft.gadgetarium.model.dto.request.*;
 import com.peaksoft.gadgetarium.model.dto.response.LoginResponse;
 import com.peaksoft.gadgetarium.model.dto.response.UserResponse;
 import com.peaksoft.gadgetarium.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@Tag(name = "User")
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -22,21 +25,25 @@ public class AuthController {
 
     UserService userService;
 
+    @Operation(summary = "Registration")
     @PostMapping("/sign-up")
     public UserResponse createUser(@RequestBody @Valid UserRequest request) {
         return userService.createUser(request);
     }
 
+    @Operation(summary = "Registration with google")
     @GetMapping("/sign-up-with-google")
     public Map<String, Object> registerWithGoogle(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
         return userService.saveWithGoogle(oAuth2AuthenticationToken);
     }
 
+    @Operation(summary = "Login")
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request) {
         return userService.login(request);
     }
 
+    @Operation(summary = "Reset password")
     @PostMapping("/resetPassword")
     public ResponseEntity<String> resetPassword(@RequestBody PasswordResetRequest request) {
         return userService.resetPassword(request);
@@ -47,6 +54,7 @@ public class AuthController {
         return userService.resetPasswordToken(request);
     }
 
+    @Operation(summary = "Update password")
     @PutMapping("/updatePassword")
     public ResponseEntity<String> updatePassword(@RequestBody UpdatePasswordRequest request) {
         return userService.updatePassword(request);
