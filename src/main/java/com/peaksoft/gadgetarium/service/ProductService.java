5fc701b0737614e6be3133b3e8240aa2ba9e2e1e
Aida <baatyrbekovaaida.dev@gmail.com>
2024-08-08 00:50:59 +0300

    product-search

--- src/main/java/com/peaksoft/gadgetarium/service/ProductService.java
+++ src/main/java/com/peaksoft/gadgetarium/service/ProductService.java
@@ -75,7 +75,6 @@ public class ProductService {
         productRepository.delete(product);
         return "Successfully deleted product by id " + id;
     }
-
     public List<Product> findProductsByName(String name) {
         return productRepository.findByProductNameContainingIgnoreCase(name);
     }
