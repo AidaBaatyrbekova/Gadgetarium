536bc87b030a5c84bfeb5ba930053e2ddcb5b4de
Aida <baatyrbekovaaida.dev@gmail.com>
2024-08-13 01:28:47 +0300

    Main-page

--- src/main/java/com/peaksoft/gadgetarium/repository/ProductRepository.java
+++ src/main/java/com/peaksoft/gadgetarium/repository/ProductRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("SELECT p FROM Product p WHERE p.discount > 0")
+    @Query("SELECT p FROM Product p WHERE p.discount > 0 and p.productStatus='SALES'")
     List<Product> findDiscounted();
 
     @Query("SELECT p FROM Product p WHERE p.productStatus = :status")
--- src/main/resources/db/db.sql
+++ src/main/resources/db/db.sql
@@ -53,11 +53,11 @@ values ('2023-07-01', 1000, 69999, 150, 1, 4, 'BLACK', '2023-06-15', '24 months'
        ('2023-06-15', 1500, 99999, 160, 1, 2, 'SILVER', '2023-05-20', '24 months', 'GB_256', 'GB_12', 'IOS', '14.5',
         'A14 Bionic', 'iPhone 12 Pro', 'SALES', '4.7', '6.1 inches', 'Single'),
        ('2023-05-30', 5, 79999, 155, 1, 1, 'WHITE', '2023-04-10', '12 months', 'GB_128', 'GB_8', 'ANDROID', '11.0',
-        'Exynos 2100', 'Galaxy S21+', 'BY_REDUCING_THE_PRICE', '4.6', '6.7 inches', 'Dual'),
+        'Exynos 2100', 'Galaxy S21+', 'SALES', '4.6', '6.7 inches', 'Dual'),
        ('2023-07-10', 20, 119999, 180, 1, 3, 'BLUE', '2023-07-01', '24 months', 'GB_512', 'GB_16', 'ANDROID', '12.0',
-        'Snapdragon 895', 'OnePlus 9 Pro', 'BY_INCREASING_THE_PRICE', '4.8', '6.7 inches', 'Dual'),
+        'Snapdragon 895', 'OnePlus 9 Pro', 'RECOMMENDATIONS', '4.8', '6.7 inches', 'Dual'),
        ('2023-06-25', 8, 89999, 165, 1, 4, 'GREEN', '2023-06-01', '24 months', 'GB_256', 'GB_12', 'ANDROID', '11.0',
-        'Kirin 9000', 'Huawei P40 Pro', 'SALES', '4.6', '6.6 inches', 'Dual');
+        'Kirin 9000', 'Huawei P40 Pro', 'RECOMMENDATIONS', '4.6', '6.6 inches', 'Dual');
 
 insert into baskets (id, user_id, quantity, total_amount, total_discount, total_sum)
 values
