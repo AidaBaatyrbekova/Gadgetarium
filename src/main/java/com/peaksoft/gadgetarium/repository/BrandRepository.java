2c3301bc7463588e6425835a644fc9f0121d7c8f
Aida <baatyrbekovaaida.dev@gmail.com>
2024-08-19 00:45:24 +0300

    addBrand

--- src/main/java/com/peaksoft/gadgetarium/repository/BrandRepository.java
+++ src/main/java/com/peaksoft/gadgetarium/repository/BrandRepository.java
@@ -1,5 +1,4 @@
 package com.peaksoft.gadgetarium.repository;
-
 import com.peaksoft.gadgetarium.model.entities.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
