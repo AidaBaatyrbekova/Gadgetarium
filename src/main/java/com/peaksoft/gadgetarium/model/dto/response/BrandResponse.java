dd086da6bf188962efa0e0a0b3d4b02cacb073a7
Aida <baatyrbekovaaida.dev@gmail.com>
2024-08-20 21:10:03 +0300

    add-brand

--- src/main/java/com/peaksoft/gadgetarium/model/dto/response/BrandResponse.java
+++ src/main/java/com/peaksoft/gadgetarium/model/dto/response/BrandResponse.java
@@ -11,6 +11,7 @@ import lombok.experimental.FieldDefaults;
 public class BrandResponse {
     Long id;
     String brandName;
+    String message;
 
     public BrandResponse(Long id, String brandName) {
         this.id = id;
