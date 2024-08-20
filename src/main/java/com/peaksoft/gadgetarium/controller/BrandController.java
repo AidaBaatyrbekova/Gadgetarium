efb40b072f5c8d11718fce15e710e7df6c668b3d
Aida <baatyrbekovaaida.dev@gmail.com>
2024-08-20 16:59:30 +0300

    add-brand

--- src/main/java/com/peaksoft/gadgetarium/controller/BrandController.java
+++ src/main/java/com/peaksoft/gadgetarium/controller/BrandController.java
@@ -1,14 +1,11 @@
 package com.peaksoft.gadgetarium.controller;
 
-import com.peaksoft.gadgetarium.model.dto.request.BrandRequest;
 import com.peaksoft.gadgetarium.model.dto.response.BrandResponse;
 import com.peaksoft.gadgetarium.service.BrandService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -18,16 +15,11 @@ import java.security.Principal;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class BrandController {
-
     BrandService brandService;
 
-    @Operation(summary = "Add new brand")
+    @Operation(summary = "add new brand")
     @PostMapping
-    public ResponseEntity<BrandResponse> addBrand(@RequestBody BrandRequest brandRequest, Principal principal) {
-        // Передаем запрос и объект Principal в сервис
-        BrandResponse brandResponse = brandService.addBrand(brandRequest.getBrandName(), principal);
-
-        // Возвращаем ответ с HTTP статусом 201 Created
-        return new ResponseEntity<>(brandResponse, HttpStatus.CREATED);
+    public BrandResponse addBrand(@PathVariable String brandName, Principal principal) {
+        return brandService.addBrand(brandName, principal);
     }
 }
