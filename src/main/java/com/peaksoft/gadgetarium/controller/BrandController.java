4dc98e710376f414c9a364f6d5ac4003e94f1337
Aida <baatyrbekovaaida.dev@gmail.com>
2024-09-01 00:39:24 +0300

    addBrand

--- src/main/java/com/peaksoft/gadgetarium/controller/BrandController.java
+++ src/main/java/com/peaksoft/gadgetarium/controller/BrandController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/brands")
+@RequestMapping("/api/brands")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class BrandController {
     BrandService brandService;
